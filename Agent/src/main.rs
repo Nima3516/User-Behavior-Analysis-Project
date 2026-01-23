e1b5eb02470e48e1e01976720441d50be2fe1c8e
Nimeshka Lakshan <65468847+Nima3516@users.noreply.github.com>
2026-01-23 18:59:40 +0530

    Main File Eddited

--- Agent/src/main.rs
+++ Agent/src/main.rs
@@ -1,3 +1,34 @@
+
+mod agent;
+mod network;
+mod logger;
+mod report;
+mod screen;
+mod dns;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    GenerateReport,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(Commands::GenerateReport) => {
+            report::generate();
+        }
+        None => {
+            agent::run();
+        }
+    }
 }
