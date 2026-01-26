e0fd6ea5462abaae873781e2fe0a8d2db75a9ab6
Nimeshka Lakshan <65468847+Nima3516@users.noreply.github.com>
2026-01-26 18:51:10 +0530

    Update logger.rs

--- Agent/src/logger.rs
+++ Agent/src/logger.rs
@@ -1,9 +1,9 @@
 use chrono::Utc;
 use std::{fs::OpenOptions, io::Write};
 
-pub fn log_network(interface: &str, sent: u64, received: u64) {
+pub fn log_network_delta(interface: &str, sent: u64, received: u64) {
     let log = format!(
-        "{} | {} | sent={} | received={}\n",
+        "{} | {} | interval_sent={} | interval_received={}\n",
         Utc::now().to_rfc3339(),
         interface,
         sent,
