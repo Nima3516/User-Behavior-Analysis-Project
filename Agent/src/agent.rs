70061bb43700c8c5398883cdf186775804e4461d
Nimeshka Lakshan <65468847+Nima3516@users.noreply.github.com>
2026-01-27 18:38:31 +0530

    Add DNS collection to agent run loop

--- Agent/src/agent.rs
+++ Agent/src/agent.rs
@@ -1,9 +1,12 @@
 use std::{thread, time::Duration};
 use crate::network;
+use crate::dns;
 
 pub fn run() {
     loop {
         network::collect();
+        dns::collect();
         thread::sleep(Duration::from_secs(5));
     }
 }
+
